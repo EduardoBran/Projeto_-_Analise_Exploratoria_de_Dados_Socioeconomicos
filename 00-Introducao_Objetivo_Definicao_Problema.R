77dcf663663b14180bedf1bd01059f3b03bbf688
EduardoBran <eduardo.ads1814@gmail.com>
2024-01-16 15:18:38 -0300

    apresentacao do projeto - objetivo e definicao do problema de negocio

---
+++ 00-Introducao_Objetivo_Definicao_Problema.R
@@ -0,0 +1,63 @@
+####  Objetivo e Definição do Problema de Negócio  ####
+
+# Configurando o diretório de trabalho
+setwd("~/Desktop/DataScience/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/17.Mini-Projeto-1_-_Analise_Exploratoria_de_Dados_Socioeconomicos")
+getwd()
+
+
+## Objetivo
+
+# - O objetivo deste mini projeto é iniciar um projeto de análise de dados. 
+#   Por onde começar? Como começar? Qual a sequência ideal de atividades?
+#   Como tomar e justificar decisões durante o processo de análise? Como reorganizar os dados para a análise?
+
+
+## Definição do Problema de Negócio
+
+# - A análise de dados socioeconômicos é fundamental para qualquer empresa compreender o cenário social e 
+#   econômico para o ambiente de negócios, compreender as diferenças entre regiões, cidades e países, a
+#   influência de indicadores nas decisões de compra dos clientes e como as mudanças no cenário podem impactar
+#   as estratégias corporativas.
+
+# - Neste mini-projeto são aplicadas técnicas de análise exploratória em dados socioeconômicos reais disponíveis
+#   publicamente. Por se tratar de dados reais, apresentam diversos problemas que são então tratados no processo
+#   de limpeza antes da análise.
+
+# - Para a análise os dados são reorganizados usando uma interessante estratégia de mudança de perspectiva dos
+#   dados. Então, 5 perguntas de negócio são respondidas através de gráficos e estatísticas.
+
+#  -> Link da fonte de dados: https://data.world/laurel/world-hapiness-report-datax
+
+
+## Perguntas de Negócio
+
+# Pergunta 1
+
+# - O Aumento do PIB per capita de um país afeta positivamente a expectativa de vida dos cidadãos ao nascer?
+#   Qual a correlação entre as duas variáveis ?
+
+# Pergunta 2
+
+# - Existe uma correlação entre a escala de vida e a conscientização do público em geral sobre a corrupção
+#   nos negócios e no governo ? Qual a correlação entre essas duas variáveis ?
+
+# Pergunta 3
+
+# - O aumento na escala de vida tem algum efeito na média de felicidade entre o público em geral ?
+#   Qual a relação entre essas duas variáveis ?
+
+# Pergunta 4
+
+# - O país com o menor índice de suporte social tem maior percepção de corrupção em relação às empresas e
+#   ao governo no páis ?
+
+# Pergunta 5
+
+# - Pessoas generosas são mais felizes ?
+
+
+# - Para responder às perguntas você deve compreender o que cada variável do dataset representa.
+#   Na sequência, deve carregar os dados, identificar eventuais problemas (que existem), limpar os dados,
+#   organizá-los e então responder às perguntas através de gráficos e estatísticas.
+
+
